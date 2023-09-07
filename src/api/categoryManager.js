08a967d8eb403e9b7f3040d0bc14566b93aa1502
bhighlander <saphrimangel@gmail.com>
2023-09-07 15:18:54 -0500

    feat: view and create categories

---
+++ src/api/categoryManager.js
@@ -0,0 +1,31 @@
+export const getAllCategories = async (token) => {
+    const response = await fetch("http://localhost:8000/categories", {
+        method: "GET",
+        headers: {
+            "Authorization": `Token ${token}`
+        }
+    });
+    return await response.json();
+}
+
+export const addCategory = async (category, token) => {
+    const response = await fetch("http://localhost:8000/categories", {
+        method: "POST",
+        headers: {
+            "Authorization": `Token ${token}`,
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(category)
+    });
+    return await response.json();
+}
+
+export const deleteCategory = async (categoryId, token) => {
+    const response = await fetch(`http://localhost:8000/categories/${categoryId}`, {
+        method: "DELETE",
+        headers: {
+            "Authorization": `Token ${token}`
+        }
+    });
+    return await response.json();
+}
---
+++ src/components/categories/ViewCategories.js
@@ -0,0 +1,62 @@
+import { useEffect, useState } from "react"
+import { addCategory, getAllCategories } from "../../api/categoryManager"
+
+export const CategoryList = ({ token }) => {
+    const [categories, setCategories] = useState([])
+    const [newCategory, setNewCategory] = useState({label: ""})
+
+    const getCategories = () => {
+        getAllCategories(token)
+            .then((categories) => {
+                setCategories(categories)
+            })
+    }
+
+    useEffect(() => {
+        getCategories()
+    }, [])
+
+    const handleAddCategory = (e) => {
+        e.preventDefault()
+        addCategory(newCategory, token)
+            .then(() => {
+                getCategories()
+            })
+    }
+
+
+    return (
+        <>
+            <h2>Categories</h2>
+            <div className="categories">
+                {categories.map((category) => {
+                    return <h3 key={category.id}>{category.label}</h3>
+                })}
+            </div>
+            {/* add a category field */}
+            <form className="form--category">
+                <h2 className="form--category__title">Add a Category</h2>
+                <fieldset>
+                    <div className="form-group">
+                        <label htmlFor="label">Label:</label>
+                        <input
+                        type="text"
+                        id="label" 
+                        name="label" 
+                        required 
+                        autoFocus 
+                        className="form-control" 
+                        value={newCategory.label}
+                        onChange={(e) => {
+                            const copy = {...newCategory}
+                            copy.label = e.target.value
+                            setNewCategory(copy)
+                        }}
+                        />
+                    </div>
+                </fieldset>
+                <button className="btn btn-primary" onClick={handleAddCategory}>Save Category</button>
+            </form>
+        </>
+    )
+}
